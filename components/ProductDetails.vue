e1839f35eaafa1ffaae8e0ce05aaf8a2a729dd70
eric <eric.blandet@gmail.com>
2023-03-28 17:41:29 +0200

    Metadata

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -13,6 +13,10 @@
         <p class="text-xl my-7">Price - CHF {{ product.price }}</p>
         <h3 class="font-bold border-b-2 mb-4 pb-2">Product description</h3>
         <p class="mb-7">{{ product.description }}</p>
+        <button class="btn flex">
+          <i class="material-icons mr-2">add_shopping_cart</i>
+          <span>Add to cart</span>
+        </button>
       </div>
     </div>
   </div>
@@ -20,6 +24,16 @@
 
 <script setup>
   const { product } = defineProps(["product"]);
+
+  useHead({
+    title: "Nuxt Dojo | Merch",
+    meta: [
+      {
+        name: "description",
+        content: "brand new merch",
+      },
+    ],
+  });
 </script>
 
 <style scoped>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "Nuxt Dojo",
+      meta: [
+        {
+          name: "description",
+          content: "fake eshop for ninjas",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
 });
